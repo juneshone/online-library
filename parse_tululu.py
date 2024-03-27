3508d76c46b1b69badefcc5407a59e79fee7d79b
juneshone <shamrova.julia@mail.ru>
2024-03-28 00:40:01 +0300

    Reduce count of requests

--- parse_tululu.py
+++ parse_tululu.py
@@ -36,14 +36,15 @@ def download_image(url, folder='images/'):
     return filepath
 
 
-def parse_book_page(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_page(url, content):
+    soup = BeautifulSoup(content, 'lxml')
 
     book_name = soup.find('h1').text.split('::', maxsplit=1)
     book_title = book_name[0].strip()
     book_author = book_name[1].strip()
+
+    book_url = soup.select('table.d_book a')[-3]['href']
+    full_book_url = urljoin('https://tululu.org/', book_url)
 
     image_url = soup.find(class_="bookimage").find('img')['src']
     full_image_url = urljoin('https://tululu.org/', image_url)
@@ -57,9 +58,10 @@ def parse_book_page(url):
     book_content = {
         'title': book_title,
         'author': book_author,
-        'url': full_image_url,
+        'image_url': full_image_url,
         'comments': book_comments,
-        'genres': book_genres
+        'genres': book_genres,
+        'book_url': full_book_url
     }
     return book_content
 
@@ -83,18 +85,20 @@ def main():
     args = parser.parse_args()
     for book_id in range(args.start_id, args.end_id):
         book_page_url = f"https://tululu.org/b{book_id}/"
-        url = f'https://tululu.org/txt.php'
         try:
-            payload = {'id': book_id}
-            response = requests.get(url, params=payload)
+            response = requests.get(book_page_url)
             response.raise_for_status()
             check_for_redirect(response)
-            book_content = parse_book_page(book_page_url)
-            book_filename = f'{book_id}. {book_content["title"]}.txt'
-            full_image_url = book_content['url']
+            book_content = parse_book_page(book_page_url, response.content)
+
+            full_image_url = book_content['image_url']
             download_image(full_image_url)
-            book_file_url = book_content['url']
-            download_txt(book_file_url, book_filename)
+
+            book_file_url = book_content['book_url']
+            book_filename = f'{book_id}. {book_content["title"]}.txt'
+            if book_file_url:
+                download_txt(book_file_url, book_filename)
+
             print('Название:', book_content['title'])
             print('Автор:', book_content['author'])
         except requests.exceptions.HTTPError as e:
